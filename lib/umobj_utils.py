8452cb94d4e0460efc634add4e8379f18a60b3d7
Liam Monahan <liam@umiacs.umd.edu>
2013-10-29 17:45:06 -0400

    This commit adds a utility function named umobj_get_bucket_key_pair_from_string, which takes a "bucket:key" string and returns the bucket name and the key name as a tuple.  If one or the other is missing, None is returned in the respecive tuple location.
    This commit also modifies all existing utilities except for cpobj to use
    this new util function.

--- lib/umobj_utils.py
+++ lib/umobj_utils.py
@@ -38,3 +38,28 @@ def umobj_init_keyboard_interrupt():
     '''Override the default SIGINT handler so that a stack trace is not printed
     to the user when a SIGINT is called'''
     signal.signal(signal.SIGINT, umobj_keyboard_interrupt_handler)
+
+
+def umobj_get_bucket_key_pair_from_string(bucket_and_key):
+    '''Return a tuple consisting of the bucket and then the key that are
+    contained in the string bucket_and_key'''
+
+    if bucket_and_key is None:
+        return (None, None)
+
+    # something like "foo:" is split into ['foo', '']
+    values = bucket_and_key.split(":", 1)
+
+    if len(values) == 2:
+        if values[0] is '':
+            values[0] = None
+        if values[1] is '':
+            values[1] = None
+        return (values[0], values[1])
+    elif len(values) == 1:
+        if values[0] is '':
+            return (None, None)
+        else:
+            return (values[0], None)
+    else:  # should never reach here
+        return (None, None)
