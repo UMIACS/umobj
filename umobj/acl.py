a55393df8be4aeb38f8e354b8bf9185efa3797a2
Liam Monahan <liam@umiacs.umd.edu>
2015-01-15 16:59:40 -0500

    Recognize predefined grantee groups (like AllUsers) when constructing grant/ACL pairs.

--- umobj/acl.py
+++ umobj/acl.py
@@ -3,14 +3,40 @@ import logging
 CANNED_ACLS = frozenset(['READ', 'WRITE', 'READ_ACP', 'WRITE_ACP',
                          'FULL_CONTROL'])
 all_users = 'http://acs.amazonaws.com/groups/global/AllUsers'
+auth_users = 'http://acs.amazonaws.com/groups/global/AuthenticatedUsers'
 
 log = logging.getLogger(__name__)
 
-def acl_pairs(acl):
+
+def get_formatted_grants(grants):
+    '''
+    Return a list of formatted grant strings
+    
+    This will pair the grantee with the permission given to the grantee.  The
+    grantee may be represented by id (e.g. liam) or by group grantees set forth
+    by the Amazon S3 API such as AllUsers and AuthenticatedUsers.
+    '''
     pairs = []
-    for grant in acl.grants:
-        pairs.append('%s:%s' % (grant.id, grant.permission))
+    for grant in grants:
+        if grant.uri == all_users:
+            grantee = "PUBLIC"
+        elif grant.uri == auth_users:
+            grantee = "AUTHENTICATED_USERS"
+        else:
+            grantee = grant.id
+        pairs.append("%s:%s" % (grantee, grant.permission))
     return pairs
+
+
+def get_formatted_grants_from_acl(acl):
+    '''Return a grant list given an ACL'''
+    return get_formatted_grants(acl.grants)
+
+
+def get_formatted_grants_from_key(key):
+    '''Return a grant list given a Key'''
+    return get_formatted_grants(key.get_acl().acl.grants)
+
 
 def split_acl(s):
     '''Return a user and CANNED_ACL split from a string'''
