a23a1ee750da9ecb82a7f7518a3482c372906bea
Liam Monahan <liam@umiacs.umd.edu>
2020-05-12 22:13:00 -0400

    When trying to download a file check if any of its directory fragements that would be recursively created exist as a file.
    This fixes a bug where we only checked if the directory key was a file,
    but this will not catch cases like this:
    
        downloading directory foo/bar/baz/
    
        We needed to check if foo, foo/bar for foo/bar/baz are files in the
        local filesystem.  We were only checking foo/bar/baz
    
    Resolves #40

--- test/test_utils.py
+++ test/test_utils.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from umobj.utils import umobj_get_bucket_key_pair_from_string
+from umobj.utils import umobj_get_bucket_key_pair_from_string, path_parts
 
 
 class TestUtils(unittest.TestCase):
@@ -49,6 +49,25 @@ class TestUtils(unittest.TestCase):
         bucket, key = umobj_get_bucket_key_pair_from_string("::")
         self.assertTrue(bucket is None)
         self.assertTrue(key == ":")
+
+    def test_path_parts(self):
+        assert sorted(list(path_parts('/tmp/foo/bar/'))) == \
+            sorted(["/", "/tmp", "/tmp/foo", "/tmp/foo/bar"])
+
+        assert sorted(list(path_parts('./tmp/foo/'))) == \
+            sorted(["./tmp/foo", "./tmp", "."])
+
+        assert sorted(list(path_parts('../tmp/foo/'))) == \
+            sorted(["../tmp/foo", "../tmp", ".."])
+
+        assert sorted(list(path_parts('tmp/foo/'))) == \
+            sorted(["tmp/foo", "tmp"])
+
+        assert list(path_parts('tmp/')) == ['tmp']
+        assert list(path_parts('tmp')) == ['tmp']
+        assert list(path_parts('tmp/foo')) == ['tmp/foo', 'tmp']
+        assert list(path_parts('/')) == ['/']
+
 
 if __name__ == '__main__':
     unittest.main()
